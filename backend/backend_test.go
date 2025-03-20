02de44b60e46eb18c11d96a33403726e89662347
JuanIgnacio Artal <jartal@kopiustech.com>
2025-03-20 00:35:53 -0300

    test again

--- backend/backend_test.go
+++ backend/backend_test.go
@@ -8,29 +8,24 @@ import (
 )
 
 func TestServer(t *testing.T) {
-	// Inicia el servidor en segundo plano
-	cmd := exec.Command("go", "run", "main.go", "database.go", "handlers.go", "routes.go", "models.go")
+	cmd := exec.Command("go run main.go database.go handlers.go routes.go models.go")
 	err := cmd.Start()
 	if err != nil {
 		t.Fatalf("Failed to start server: %v", err)
 	}
 
-	// Espera un momento para asegurarse de que el servidor esté listo
-	time.Sleep(2 * time.Second) // Ajusta el tiempo según sea necesario
+	time.Sleep(2 * time.Second)
 
-	// Realiza la solicitud GET
 	resp, err := http.Get("http://localhost:8080/bookings")
 	if err != nil {
 		t.Fatalf("Failed to make GET request: %v", err)
 	}
 	defer resp.Body.Close()
 
-	// Verifica la respuesta del servidor
 	if resp.StatusCode != http.StatusOK {
 		t.Errorf("Expected status 200 OK, got %v", resp.StatusCode)
 	}
 
-	// Detiene el servidor después de la prueba
 	err = cmd.Process.Kill()
 	if err != nil {
 		t.Fatalf("Failed to kill server: %v", err)
